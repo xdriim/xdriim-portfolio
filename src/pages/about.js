2cef3cc4e3e498369e29deadb36a67040f662dcf
xdriim <adriivallejo11@gmail.com>
2024-02-07 18:39:50 +0100

    First

---
+++ src/pages/about.js
@@ -0,0 +1,94 @@
+import Layout from "@/components/Layout";
+import Head from "next/head";
+import Image from "next/image";
+import profile from "../../public/images/profile/fotoConPerro.jpg";
+import { useInView, useMotionValue, useSpring } from "framer-motion";
+import { useEffect, useRef } from "react";
+import Skills from "@/components/Skills";
+import Experience from "@/components/Experience";
+import Education from "@/components/Education";
+import AnimatedText from "@/components/AnimatedText";
+import TransitionEffect from "@/components/TransitionEffect";
+
+function AnimatedNumberFramerMotion({ value }) {
+  const ref = useRef(null);
+  const motionValue = useMotionValue(0);
+  const springValue = useSpring(motionValue, { duration: 3000 });
+  const isInView = useInView(ref, { once: true });
+  useEffect(() => {
+    if (isInView) {
+      motionValue.set(value);
+    }
+  }, [motionValue, value, isInView]);
+
+  useEffect(
+    () =>
+      springValue.on("change", (latest) => {
+        if (ref.current && latest.toFixed(0) <= value) {
+          ref.current.textContent = latest.toFixed(0);
+        }
+      }),
+    [springValue, value]
+  );
+
+  return <span ref={ref} />;
+}
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>Portfolio Built with Nextjs | About Page</title>
+        <meta name="description" content="Read about me, a developer very motivated to develop all his skills and ready for know new things" />
+      </Head>
+      <TransitionEffect />
+      <main
+        className={`flex  w-full flex-col items-center justify-center dark:text-light`}
+      >
+        <Layout className="pt-16">
+          <AnimatedText
+            text="About me"
+            className="mb-16 !text-8xl !leading-tight lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8"
+          />
+
+          <div className="grid w-full grid-cols-8 gap-16 sm:gap-8">
+            <div className="col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 
+            md:col-span-8">
+              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75">
+                BIOGRAPHY
+              </h2>
+              <p className="font-medium ">
+                Hi, I'm <strong>Adrià</strong>, a programmer who has learned to code in Kotlin and continued the journey with JavaScript, 
+                focusing on web applications using various frameworks. My expertise lies in crafting efficient and versatile 
+                solutions, and I'm eager to explore new technologies while contributing to the success of upcoming projects.
+              </p>
+            </div>
+            {/* <div className="relative col-span-3 h-max rounded-2xl border-2 border-solid border-dark 
+            bg-light p-8 dark:border-light dark:bg-dark
+            xl:col-span-4 md:col-span-8 md:order-1
+            ">
+              <div
+                className="absolute  top-0 -right-3 -z-10 h-[103%] w-[102%]  rounded-[2rem] rounded-br-3xl 
+                bg-dark
+        dark:bg-light  "
+              />
+              <Image
+                className="h-auto w-full rounded-2xl"
+                src={profile}
+                alt="Codebucks"
+                sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw"
+                priority
+              />
+            </div> */}
+          </div>
+
+          <Skills />
+          {/* <Experience /> */}
+          <Education />
+        </Layout>
+      </main>
+    </>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,81 @@
+import AnimatedText from "@/components/AnimatedText";
+import { HireMe } from "@/components/HireMe";
+import { LinkArrow } from "@/components/Icons";
+import Layout from "@/components/Layout";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+// import lightBulb from "../../public/images/svgs/miscellaneous_icons_1.svg";
+import profilePic from "../../public/images/profile/filtroAzul.png";
+import TransitionEffect from "@/components/TransitionEffect";
+
+
+export default function Home() {
+  
+  return (
+    <>
+      <Head>
+        <title>Portfolio XdriiM</title>
+        <meta
+          name="description"
+          content="Check the portfolio of XdriiM, a web app developer who starts his aventure now"
+        />
+      </Head>
+
+      <TransitionEffect />
+      <article
+        className={`flex min-h-screen items-center text-dark dark:text-light sm:items-start`}
+      >
+        <Layout className="!pt-0 md:!pt-16 sm:!pt-16">
+          <div className="flex w-full items-start justify-center md:flex-col">
+            {/* <div className="w-1/4 lg:hidden md:inline-block md:w-full">
+              <Image
+                src={profilePic}
+                alt="XdriiM"
+                className="h-auto w-full"
+                sizes="100vw"
+                priority
+              />
+            </div> */}
+            <div className="flex w-3/4 flex-col items-center self-center lg:w-full lg:text-center">
+              <AnimatedText
+                text="Adrià Vallejo Proaño"
+                className="!text-left !text-6xl xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl"
+              />
+              <p className="my-4 text-base font-medium md:text-sm sm:!text-xs">
+              Passionate web developer with a keen interest in programming, currently delving into Artificial Intelligence and Big Data studies. Striving to create innovative and scalable technological solutions that drive positive impact in the digital era.
+              </p>
+              <div className="mt-2 flex items-center self-start lg:self-center">
+                <Link
+                  // whileHover={{
+                  //   cursor: `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='40' height='48' viewport='0 0 100 100' style='font-size:24px;'><text y='50%'>👆</text></svg>"), auto`,
+                  // }}
+                  href="/CV_Vallejo Proaño, Adria_DAW.pdf"
+                  target={"_blank"}
+                  className={`flex items-center rounded-lg border-2 border-solid bg-dark p-2.5 px-6 text-lg font-semibold
+            capitalize text-light hover:border-dark hover:bg-transparent hover:text-dark 
+            dark:bg-light dark:text-dark dark:hover:border-light dark:hover:bg-dark dark:hover:text-light
+            md:p-2 md:px-4 md:text-base
+             `}
+                  download
+                >
+                  Resume <LinkArrow className="ml-1 !w-6 md:!w-4" />
+                </Link>
+
+                <Link
+                  href="mailto:adriivallejo11@gmail.com"
+                  className="ml-4 text-lg font-medium capitalize text-dark underline 
+                  dark:text-light md:text-base"
+                >
+                  Contact
+                </Link>
+              </div>
+            </div>
+          </div>
+        </Layout>
+
+        {/* <HireMe /> */}
+      </article>
+    </>
+  );
+}
